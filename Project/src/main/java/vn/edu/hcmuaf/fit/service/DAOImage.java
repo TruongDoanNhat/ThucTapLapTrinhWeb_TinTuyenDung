198102ea1ff3622f87d904b0dfcced82edbfca8c
nguyen dinh nguyen <20130340@st.hcmuaf.edu.vn>
2023-06-18 16:36:38 +0700

    sửa trang chủ, thêm đường dẫn hình ảnh. sửa database

--- Project/src/main/java/vn/edu/hcmuaf/fit/service/DAOImage.java
+++ Project/src/main/java/vn/edu/hcmuaf/fit/service/DAOImage.java
@@ -7,10 +7,18 @@ import java.util.List;
 
 public class DAOImage {
     private static List<Image> imageList = null;
+
     public static List<Image> listImage() {
         String query = "select * from image";
         imageList = JDBIConnector.get().withHandle(handle -> handle.createQuery(query)
                 .mapToBean(Image.class).list());
         return imageList;
+    }
+
+    public static String getURL(int idImage) {
+        String query = "SELECT url FROM image WHERE id = ?";
+        return JDBIConnector.get().withHandle(handle -> handle.createQuery(query)
+                .bind(0, idImage)
+                .mapTo(String.class).stream().findFirst().get());
     }
 }
